1ee0ed52496ea270f4e988f13eb9bc5c9d752712
Andy <lyle.kahney@mac.com>
2014-11-10 17:47:25 -0800

    lots of stuff

--- AdventuresFluffles.js
+++ AdventuresFluffles.js
@@ -11,29 +11,47 @@ var main = {
         game.add.sprite(0,0, 'background') 
         this.player = game.add.sprite(100, 245, 'cat');
         game.physics.arcade.enable(this.player);
-
-    this.player.body.gravity.y = 200;
+        this.player.body.collideWorldBounds = true;
+       
+        this.player.body.gravity.y = 200;
         
         this.ground = game.add.sprite(0,500, 'ground');
+        game.physics.arcade.enable(this.ground);  
+        this.ground.body.immovable = true;
+    var up = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+        up.onDown.add(this.jump, this);
         
-        game.physics.arcade.enable(this.ground);
-
-    
-    var space = 
-    game.input.keyboard.addKey(Phaser.Keyboard.UP);
-        space.onDown.add(this.jump, this);
+    var left = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+       left.onDown.add(this.moveleft, this);
+        left.onUp.add(this.stopmoving, this);
+        
+    var right = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+       right.onDown.add(this.moveright, this);
+        right.onUp.add(this.stopmoving, this);
         
     
-        game.physics.arcade.overlap(this.player, this.ground, this.restartGame, null, this);
+    game.physics.arcade.overlap(this.player, this.ground, this.restartGame,           null, this);
     },
   
     jump: function() {
-        this.player.body.velocity.y = -250;
+        this.player.body.velocity.y = -225;
+    },
+    
+    moveleft: function() {
+        this.player.body.velocity.x = -175;
+    },
+    
+     moveright: function() {
+        this.player.body.velocity.x = 175;
+    },
+    
+    stopmoving: function() {
+        this.player.body.velocity.x = 0;
     },
     
     
-    restartGame: function() {
-        game.state.start('default');    
+    update: function() {
+        game.physics.arcade.collide(this.player, this.ground);
     },
 };
 
